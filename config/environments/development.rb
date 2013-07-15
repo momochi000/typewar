e2b8e32182470f8ee2a9026d84ca7ca20329eaa7
Albert Peng <albert.peng@cureus.com>
2013-07-14 20:31:34 -0700

    Extends cache control expiration to fix sprite flickering

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,8 @@ Typewar::Application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.static_cache_control = "public, max-age=2592000"
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
