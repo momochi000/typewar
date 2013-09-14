4bae148c822c5062af3d5395b995d9166cef08b7
momochi000 <zacherychin@yahoo.com>
2013-09-14 16:17:22 -0700

    Create character model

---
+++ app/models/character.rb
@@ -0,0 +1,22 @@
+require 'json'
+
+class Character < ActiveRecord::Base
+  serialize :char_sheet, JSON
+  serialize :status, JSON
+  serialize :vocabulary, JSON
+
+  def self.generate_vocabulary
+    15.times.map{|itor|
+      [ Faker::Address.city,
+        Faker::Address.state,
+        Faker::Company.catch_phrase
+      ]
+    }.flatten.to_json
+  end
+
+  # Return a json to send to the server and be converted into a game character
+  # TODO: move this to a decorator
+  def to_backbone
+    self.to_json
+  end
+end
