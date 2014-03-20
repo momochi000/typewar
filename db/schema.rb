0a59312a4f875b9e649c22ffca0686a8baf2d84b
momochi000 <zacherychin@yahoo.com>
2014-03-19 22:19:30 -0700

    db:create db:migrate after switching to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130915034726) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "characters", force: true do |t|
     t.string   "name"
     t.string   "char_class"
