94a04acf9b92217872dbef1a6c4edcd9771a24fe
momochi000 <momochi000@gmail.com>
2014-04-29 23:02:32 -0700

    Refactor a bit the initialization of the game

--- app/assets/javascripts/Typewar.js
+++ app/assets/javascripts/Typewar.js
@@ -1,19 +1,64 @@
+// TODO: wrap this in a closure
+
 Typewar.Engine = {};
+Typewar.Data = {};
+Typewar.Views = {};
+Typewar.Initializer = (function (Typewar){
+  var init,
+    // private class methods
+      setupContainer, initCrafty, initBox2d, initSceneManager,
+    // private class variables
+      container_selector, options, viewportWidth, viewportHeight;
+  
+  init = function (container, opts){
+    container_selector = container;
+    options = opts;
 
-Typewar.initGame = function (viewportWidth, viewportHeight){
-  var world, PTM_RATIO;
-  Crafty.init(viewportWidth, viewportHeight);
-  Crafty.viewport.init(viewportWidth, viewportHeight);
-  Crafty.background("blue");
-  this.viewportHeight = viewportHeight;
-  this.viewportWidth = viewportWidth;
+    setupContainer();
+    initCrafty();
+    initBox2d();
+    initSceneManager();
+  };
 
-  PTM_RATIO = 2; //32
-  Crafty.box2D.init(0, 10, PTM_RATIO, true);
-  world = Crafty.box2D.world;
-  //Crafty.box2D.showDebugInfo(); //Start the Box2D debugger
+  initBox2d = function (){
+    var PTM_RATIO = 2; //32
+    Crafty.box2D.init(0, 10, PTM_RATIO, true);
+    world = Crafty.box2D.world;
+    //Crafty.box2D.showDebugInfo(); //Start the Box2D debugger
+  };
 
-  Typewar.Engine.scenemanager = new SceneManager({scene_map: {battle_scene: ProtoBattleScene, battle_over: BattleOverScene}});
+  initCrafty = function (){
+    Crafty.init(viewportWidth, viewportHeight);
+    Crafty.viewport.init(viewportWidth, viewportHeight);
+    Crafty.background("blue");
+  };
+
+  initSceneManager = function (){
+    Typewar.Engine.scenemanager = new SceneManager({scene_map: {battle_scene: ProtoBattleScene, battle_over: BattleOverScene}});
+  };
+
+  setupContainer = function (){
+    container_selector = container_selector       || 'body';
+    viewportWidth      = options.viewportWidth    || 1080;
+    viewportHeight     = options.viewportHeight   || 480;
+
+    Typewar.Engine.ContainerSelector = container_selector;
+    Typewar.Engine.$container = $(container_selector);
+
+    Typewar.Engine.$container.css({
+      "margin": "auto",
+      "width" : (viewportWidth + 'px')
+    });
+  };
+
+  return {init: init};
+  
+})(Typewar);
+
+
+
+Typewar.initGame = function (container_selector, options){
+  Typewar.Initializer.init(container_selector, options);
 };
 
 Typewar.startGame = function (){ 
