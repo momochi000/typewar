ae4546c723cb883d1b5428f5f3ea41f720e5e048
momochi000 <momochi000@gmail.com>
2014-04-29 23:01:11 -0700

    Add javascript state machine

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
+//= require state-machine
 //= require underscore
 //= require underscore.mixin.deepExtend
 //= require backbone
