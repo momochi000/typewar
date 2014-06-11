b017e826eb71e558fb34e1ebbab2c24823711b55
momochi000 <momochi000@gmail.com>
2014-06-10 17:57:27 -0700

    Refactor NPC AI

--- app/assets/javascripts/Typewar/components/brains/NPCBrainComponent.js
+++ app/assets/javascripts/Typewar/components/brains/NPCBrainComponent.js
@@ -11,14 +11,17 @@ Crafty.c("NPCBrain", {
 
   remove: function (){
     this._clearBattleTimer();
+    this._unbindAIOnPause();
   },
 
   activateAI: function (){
-    this._activate();
+    this._is_active = true;
+    if(!this._current_target){ return false; }
+    if(!this.battle_timer){ this._initBattleTimer(); }
   },
 
   deactivateAI: function (){
-    this._deactivate();
+    this._is_active = false;
   },
 
   isActive: function (){
@@ -27,34 +30,30 @@ Crafty.c("NPCBrain", {
 
   // private
 
-  _activate: function (){
-    var self = this;
-
-    this._is_active = true;
-    if(!this._current_target){ return false; }
-    if(!this.battle_timer){
-      this.battle_timer = window.setInterval(function() {
-        if(self._is_active){
-          self.initiateAttackOn(self._current_target);
-        }
-      }, 2000);
-    }
+  _aiCycle: function (){
+    // check to see which skills are available.
+    // decide whether or not to fire one.
+    this.initiateAttackOn(this._current_target);
   },
 
   _bindAIOnPause: function (){
     var self = this;
-    this.bind("Pause", function (){ self._deactivate(); });
-    this.bind("Unpause", function (){
-      if(self._is_active === false){ self._is_active = true; }
-    });
+    this.bind("Pause", function (){ self.deactivateAI(); });
+    this.bind("Unpause", function (){ self.activateAI(); });
   },
 
   _clearBattleTimer: function (){
     if(this.battle_timer){ window.clearInterval(this.battle_timer); }
   },
 
-  _deactivate: function (){
-    this._is_active = false;
+  _initBattleTimer: function (){
+    var self = this;
+
+    this.battle_timer = window.setInterval(function() {
+      if(self._is_active){
+        self._aiCycle();
+      }
+    }, 100);
   },
 
   _unbindAIOnPause: function (){
