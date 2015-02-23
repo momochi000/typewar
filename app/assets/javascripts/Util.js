7aa616c2059ac8a0a50273853f3ff558adde90ab
momochi000 <momochi000@gmail.com>
2015-02-22 21:11:55 -0800

    Add isArray and isObject to typewar util

--- app/assets/javascripts/Util.js
+++ app/assets/javascripts/Util.js
@@ -1,8 +1,24 @@
 Typewar.Util = _.extend({}, Typewar.Util, ( function (){
-  var coinToss, randomInt;
+  var coinToss, randomInt, isArray, isObject;
 
   coinToss = function (){
     return ( (Math.floor(Math.random()*2)) === 1 );
+  };
+
+  isArray = function (arg){
+    if( Object.prototype.toString.call( arg ) === '[object Array]' ) {
+      return true;
+    }else{
+      return false;
+    }
+  };
+
+  isObject = function (arg){
+    if( Object.prototype.toString.call( arg ) === '[object Object]' ) {
+      return true;
+    }else{
+      return false;
+    }
   };
 
   randomInt = function (start, end){
@@ -14,6 +30,8 @@ Typewar.Util = _.extend({}, Typewar.Util, ( function (){
 
   return {
     coinToss: coinToss,
+    isArray: isArray,
+    isObject: isObject,
     randomInt: randomInt
   };
 })());
