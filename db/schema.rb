f96da815f8e2818f58bafa913ba132554379eba8
momochi <momochi000@gmail.com>
2015-05-29 15:17:35 -0700

    Schema update from rebuilding db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140412061436) do
+ActiveRecord::Schema.define(version: 20140526073713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20140412061436) do
     t.text     "status",     default: ""
     t.text     "properties"
   end
-
 
   create_table "texts", force: true do |t|
     t.text     "content"
