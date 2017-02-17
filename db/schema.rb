4b41762da9364a2fed8ac58d39fa3d70fe7e3897
momochi <momochi000@gmail.com>
2017-02-16 18:21:03 -0800

    schema change with db:create

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,7 +15,7 @@ ActiveRecord::Schema.define(version: 20140526073713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "characters", force: true do |t|
+  create_table "characters", force: :cascade do |t|
     t.string   "name"
     t.string   "char_class"
     t.text     "stats"
@@ -26,7 +25,7 @@ ActiveRecord::Schema.define(version: 20140526073713) do
     t.text     "properties"
   end
 
-  create_table "texts", force: true do |t|
+  create_table "texts", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
