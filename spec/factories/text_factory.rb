d569aa2d19a7c6896b406f815965f8ea848eaf9c
momochi <momochi000@gmail.com>
2017-03-21 21:26:57 -0700

    Add a hipster text factory to Text
    to generate a bit more realistic dummy text

--- spec/factories/text_factory.rb
+++ spec/factories/text_factory.rb
@@ -3,6 +3,10 @@ FactoryGirl.define do
     content { Faker::Lorem.paragraphs.join('')}
   end
 
+  factory :hipster_text, :parent => :text do
+    content { Faker::Hipster.paragraphs(2).join('') }
+  end
+
   factory :test_text, :parent => :text do
     content "The quick brown fox jumped over the slow lazy dog"
   end
