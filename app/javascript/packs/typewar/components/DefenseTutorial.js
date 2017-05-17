54adbb1c3a51fb61b0b8362f2b40d00fd3f21504
momochi <momochi000@gmail.com>
2017-05-16 21:06:13 -0700

    Add a UI notice for the defense tutorial
    Indicates how many attacks left to block.

--- app/javascript/packs/typewar/components/DefenseTutorial.js
+++ app/javascript/packs/typewar/components/DefenseTutorial.js
@@ -1,3 +1,27 @@
+var Handlebars = require('handlebars');
+import Backbone from 'backbone'
+
+var DefTutorialNotificationView = Backbone.View.extend({
+  tagName: 'div',
+  className: 'defense-tutorial-notification',
+  id: 'defense-tutorial-notification',
+  _templateId: '#defense-tutorial-notif-template',
+
+  initialize: function (){
+    console.log("DEBUG: initializing tutorial notification view");
+    this._template = Handlebars.compile($(this._templateId).html());
+  },
+
+  render: function (count){
+    var opts = {count: count};
+    console.log("DEBUG: rendering tutorial notif view.  With count -> ",opts);
+    this.$el.html(this._template(opts));
+    if($("#"+this.id).length <1){ // insert into page if it's not there
+      $("#typewar-game").prepend(this.$el);
+    }
+  }
+});
+
 Crafty.c("DefenseTutorial", {
   init: function (){ 
     this._defendedCount = 0;
@@ -5,7 +29,13 @@ Crafty.c("DefenseTutorial", {
 
   defenseTutorial: function (count){
     this._defendedGoal = count;
+    this._view = new DefTutorialNotificationView();
+    this._view.render(count);
     return this;
+  },
+
+  cleanup: function (){
+    this._view.remove();
   },
 
   getDefenseGoal: function (){
@@ -14,6 +44,10 @@ Crafty.c("DefenseTutorial", {
 
   getDefendedCount: function (){
     return this._defendedCount;
+  },
+
+  getView: function (){
+    return this._view;
   },
 
   incrementDefendedCount: function (){
