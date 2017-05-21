b9c050ca97872e6146aa704d8a7a421523522876
momochi <momochi000@gmail.com>
2017-05-20 19:38:32 -0700

    WIP REBASE ME create a scene transition

--- app/javascript/packs/typewar/systems/player_die_lose_system.js
+++ app/javascript/packs/typewar/systems/player_die_lose_system.js
@@ -6,9 +6,7 @@ export function playerDieLoseSystem(Crafty){
   player = Crafty("BattlePlayer");
   if(player.length==0){ return; }
   if(player.getHP() <= 0){
-    // LOSE CONDITION MET, 
-    // somehow, this has to get back to the scene
-    // Likely we'll just trigger an event here
+
     console.log("DEBUG: PLAYER DIED> GAME LOST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     Crafty.trigger(SCENE_TRANSITION_EVT, BATTLE_DEFEAT_COND);
   }
