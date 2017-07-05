6ae49884e685812d1d09201f1de81dfc6d36d345
momochi <momochi000@gmail.com>
2017-07-05 15:33:26 -0700

    Add rack-cors gem
    to deal with cross origin issues with distributing assets on cloudfront

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module Typewar
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.midddleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get :post :options :head]
+      end
+    end
   end
 end
